0ca6fa70045d1d54c4e9b5ab48555d592ef211f2
killvung <killvung@gmail.com>
2021-04-20 12:51:45 -0500

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css'
+import "govuk-frontend/govuk/all.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import Head from 'next/head'
+
+import Layout from '../components/layout'
+import Accordion from '../components/Accordion'
+import styles from '../styles/Home.module.css'
+
+const TITLE = "?";
+
+export default function Home() {
+  // By default NextJS performs server rednering thus initAll() would have error since document is not existed yet
+  // temporary solution would be wait until the page has loaded on the client side then call initAll()
+  useEffect(() => {
+    const GOVUKFrontend = require('govuk-frontend');
+    GOVUKFrontend.initAll();
+  });
+
+  return (
+    <Layout>
+      <Head>
+        <title>{TITLE}</title>
+      </Head>
+      <main className={styles.main}>
+        {/* Enable Govuk JS interaction anything below this element */}
+        <div className="js-enabled">
+          <Accordion />
+        </div>
+      </main >
+    </Layout>
+  )
+}
