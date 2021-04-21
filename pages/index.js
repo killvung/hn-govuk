e5917d28fc074c95a6fbfa46d4127eea944fa4ef
killvung <killvung@gmail.com>
2021-04-21 13:01:53 -0500

    fix rootid

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,7 @@ export default function Home() {
                   timestamp={time}
                   title={title}
                   url={url}
+                  rootId={descendants}
                 />
               </li>
             ))}
