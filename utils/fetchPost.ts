1d1ab50bbbccdd379d59dc07b23aa8f27db9545e
killvung <killvung@gmail.com>
2021-04-23 17:05:18 -0500

    i dumb

--- utils/fetchPost.ts
+++ utils/fetchPost.ts
@@ -1,13 +1,13 @@
+// TODO: Differentiate between Story and Comment for Post
 type Post = {
-    by: string;
-    descendants: number;
     id: number;
-    kids: [number];
-    time: number;
-    score: number;
+}
+
+interface Story extends Post {
     title: string;
-    type: string;
-    url: string;
+}
+interface Comment extends Post {
+    text: string;
 }
 
 const HACKER_NEWS_API_BASE_URL = "https://hacker-news.firebaseio.com/v0/";
@@ -15,7 +15,7 @@ const HACKER_NEWS_API_BASE_URL = "https://hacker-news.firebaseio.com/v0/";
 const constructPostURL = (postId: number) =>
     (`${HACKER_NEWS_API_BASE_URL}/item/${postId}.json`)
 
-export const fetchPost = async (postId: number): Promise<Post> => {
+export const fetchPost = async (postId: number): Promise<Story | Comment> => {
     const response = await fetch(constructPostURL(postId));
     return await response.json();
 }
