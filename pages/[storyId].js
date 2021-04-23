099850698287d2b2e0815d9655acdeada6a5e72d
killvung <killvung@gmail.com>
2021-04-23 14:47:04 -0500

    remove

--- pages/[storyId].js
+++ pages/[storyId].js
@@ -2,13 +2,6 @@ import Layout from '../components/layout'
 import CommentContainer from '../components/Comment/CommentContainer'
 import styles from '../styles/Home.module.css'
 
-export async function getServerSideProps(context) {
-    const storyId = context.params.storyId
-    return {
-        props: { storyId }
-    }
-}
-
 export default function Comment({ storyId }) {
     return (
         <Layout>
