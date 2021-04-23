6ccd63e5c71d1fb64eceebd35f07870b4dd7818b
killvung <killvung@gmail.com>
2021-04-23 12:23:47 -0500

    CommentContainer drafted

--- pages/[storyId].js
+++ pages/[storyId].js
@@ -1,9 +1,20 @@
+import Layout from '../components/layout'
 import { useRouter } from 'next/router'
 
-const Story = () => {
+import CommentContainer from '../components/Comment/CommentContainer'
+import styles from '../styles/Home.module.css'
+
+export default function Comment() {
     const router = useRouter()
     const { storyId } = router.query
-    return <p>Story: {storyId}</p>
+    return (
+        <Layout>
+            <main className={styles.main}>
+                <p>Story: {storyId}</p>
+                <div className="js-enabled">
+                    <CommentContainer />
+                </div>
+            </main >
+        </Layout>
+    )
 }
-
-export default Story
