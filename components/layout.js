8f6fddad264c73b7b5f5d03dee4da3485319e82e
killvung <killvung@gmail.com>
2021-04-23 11:57:40 -0500

    Layout

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import Header from '../components/header.tsx'
 import Footer from '../components/footer.tsx'
 import Navigation from '../components/Navigation.tsx';
 
-export default function Layout({ children }) {
-    return (
-        <>
-            <Header />
-            <Navigation>
-                {children}
-            </Navigation>
-            <Footer />
-        </>
-    );
-}
+const Layout = ({ children }) => (
+    <>
+        <Header />
+        <Navigation>
+            {children}
+        </Navigation>
+        <Footer />
+    </>
+);
+
+export default Layout;
