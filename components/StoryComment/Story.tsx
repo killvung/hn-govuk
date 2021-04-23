4faf27140c09333ee070ee1b44969e6e841ad653
killvung <killvung@gmail.com>
2021-04-23 18:45:10 -0500

    return

--- components/StoryComment/Story.tsx
+++ components/StoryComment/Story.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StoryProps } from '../../models/props';
 
-const Story: React.FC<StoryProps> = ({ title, url, score, author, timestamp, descendants, rootId, kids }) => (
-    <div>
-        Story Placeholder
-        {title} {url} {score} {author} {timestamp} {descendants} {rootId}
-    </div>
-);
+const Story: React.FC<StoryProps> = ({ title, url, score, author, timestamp, descendants, rootId }) => {
+
+    return (
+        <div>
+            Story Placeholder
+            {title} {url} {score} {author} {timestamp} {descendants} {rootId}
+        </div>
+    );
+}
 
 export default Story;
