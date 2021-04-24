626281fc17f29d0e157adfbe45eeb16fed673773
killvung <killvung@gmail.com>
2021-04-24 18:07:27 -0500

    test case drafted

---
+++ components/Root/test/RootContainer.test.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import shallow from 'react-test-renderer/shallow';
+// import RootContainer from '../RootContainer';
+// import { useRouter } from 'next/router';
+// jest.mock('next/router');
+
+// import mockDataTopStories from './mockDataTopStories.json';
+// import mockDataJobStories from './mockDataJobStories.json';
+// import mockDataAskStories from './mockDataAskStories.json';
+
+beforeEach(() => {
+
+});
+
+test('RootContainer - render default correctly', () => {
+    // TODO:
+});
+
+test('RootContainer - render ask stories correctly', () => {
+    // TODO:
+});
+
+test('RootContainer - render job stories correctly', () => {
+    // TODO:
+});
