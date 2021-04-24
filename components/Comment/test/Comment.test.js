fe298ea5ea81ebb521b361d9c72ad8912b08bd32
killvung <killvung@gmail.com>
2021-04-24 18:13:32 -0500

    Refactor Story and Comment

---
+++ components/Comment/test/Comment.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Comment from '../Comment';
+
+test('Comment - render successfully', () => {
+
+});
+
+test('Comment - Multiple top level comments', () => {
+
+});
+
+test('Comment - Nested Comments', () => {
+
+});
