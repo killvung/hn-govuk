083a4b31ecfce6b6243099b72206eddeb8256011
killvung <killvung@gmail.com>
2021-04-25 16:14:45 -0500

    added back files

--- components/Comment/CommentContainer.tsx
+++ components/Comment/CommentContainer.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const CommentContainer: React.FC = () => (
-    <div>Comment Container Placeholder</div>
-);
+import fetchPost from '../../utils/fetchPost';
+import { CommentContainerProps } from '../../models/props';
+import Comment from './Comment';
+
+const CommentContainer: React.FC<CommentContainerProps> = ({ commentId }) => {
+    const [comment, setComment] = useState<any>();
+
+    useEffect(() => {
+        fetchPost(commentId).then(comment => {
+            setComment(comment);
+        })
+
+    }, [commentId]);
+
+    if (!comment) {
+        return <span />
+    }
+    const { by, time, text, kids } = comment;
+    return (
+        <div>
+            {/* Comments */}
+            <div>
+                <Comment
+                    author={by}
+                    timestamp={time}
+                    comment={text}
+                    children={kids}
+                />
+            </div>
+
+        </div>
+    );
+}
 
 export default CommentContainer;
