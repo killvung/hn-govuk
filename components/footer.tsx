1ee3061f7f2b67fcc860d041d6a7bb891dbe7023
killvung <killvung@gmail.com>
2021-04-27 14:00:45 -0500

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import styles from '../styles/Home.module.css'
 
-const AUTHOR = "?";
+const AUTHOR = "killvung";
 
 const Footer: React.FC = () => (
-    <div className={styles.footer}>
-        {AUTHOR}
+    <div>
+        <footer className="govuk-footer " role="contentinfo">
+            <div className="govuk-width-container ">
+                <div className="govuk-footer__meta">
+                    <div className="govuk-footer__meta-item">
+                        <a className="govuk-footer__link" href="https://design-system.service.gov.uk/" rel="license">GOV.UK Design System</a>
+                        <br />
+                        {AUTHOR} <a className="govuk-footer__link" href="https://www.gnu.org/licenses/copyleft.en.html/">(ɔ) 2021</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 )
 
